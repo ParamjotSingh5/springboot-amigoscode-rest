2691b579e6a078171bcb444d098e59972a117294
Paramjot Singh <paramjotdhiman5@gmail.com>
2023-09-19 01:35:01 +0530

    Revert "Revert "feat: intiali setup""
    This reverts commit 4d757973c2fb47e2565379c4cee2f82af850b662.

---
+++ src/test/java/com/paramjot/amigoscoderestapi/AmigoscodeRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paramjot.amigoscoderestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmigoscodeRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
